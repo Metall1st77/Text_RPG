bcee97a3ffb0b027b91d336380aad4eddc5121d7
Evgenii <Metall1st@github.com>
2019-08-05 09:33:53 +0300

    Trying to come up with spreading biome stuff

--- field.py
+++ field.py
@@ -7,8 +7,9 @@ from creatures.creature import creature
 
 class field():
     char_model = 'X'
+    no_model = '0'
 
-    biomes = ('desert', 'snow', 'swamp', 'ground')
+    biomes = ('desert', 'snow', 'swamp', 'ground', 'forest')
 
     symbs_for_biomes = { 'desert' : '.',
                          'snow'   : '\'',
@@ -19,11 +20,11 @@ class field():
     level_enemy_count = [3, 4, 6, 8, 11, 15, 17, 19, 20, 21]
 
     full_field = []
-    f_rows = 50
+    f_rows = 70
     f_cols = 70
 
     visible_field = []
-    v_rows = 20
+    v_rows = 30
     v_cols = 30
 
     random_percentage = 0.25
@@ -35,20 +36,23 @@ class field():
 
     def __init__(self, character, level = 0, load = False):
         self.enemies = self.level_enemy_count[level]
-        biomes = self.__biomes_area_count()
         self.current_level = level
         self.character = character
         if load:
             self.load_field()
         else:
-            self.__create_field(self.character, self.enemies, biomes)
+            self.__create_field()
 
     def load_field(self):
         return
 
-    def __create_field(self, character, enemies, biomes):
+    def __create_field(self):
+        self._fill_with_nothing(self.no_model)
+
+        biomes = self.__biomes_area_count()
         starting_spots = self.__define_starting_spots(biomes)
-        self.__fill_field()
+
+        self.__spreading(starting_spots, biomes)
 
         for row in range(self.v_rows):
             self.visible_field.append([])
@@ -72,20 +76,81 @@ class field():
         central_point = { 'x' : self.f_rows // 2,
                           'y' : self.f_cols // 2 }
         diagonal = math.sqrt(math.pow(self.f_rows, 2) + math.pow(self.f_cols, 2))
-        radius = int(random.randint(2, 3) / 10 * diagonal)
+        radius = int(random.randint(20, 30) / 100 * diagonal)
         angle = (2 * math.pi) / number_of_points
         starting_spots = []
         for i in range(number_of_points):
             X = radius * math.cos(angle * i) + central_point['x']
             Y = radius * math.sin(angle * i) + central_point['y']
-            spot = (X, Y)
+            spot = (int(X), int(Y))
             starting_spots.append(spot)
         return starting_spots
 
+    def _fill_with_nothing(self, symbol):
+        for row in range(self.f_rows):
+            self.full_field.append([])
+            for col in range(self.f_cols):
+                self.full_field[row].append(symbol)
 
-    def _circle(self, x, start_x = 0, start_y = 0, radius = 0):
-        y = math.sqrt(math.pow(radius, 2) - math.pow((x - start_x), 2)) + start_y
-        return y
+    def __spreading(self, starting_spots, biomes):
+        full = False
+
+        i = 0
+        for spot in biomes.keys():
+            x, y = starting_spots[i][0], starting_spots[i][1]
+            self.full_field[x][y] = self.symbs_for_biomes[spot]
+            i += 1
+
+        while not full:
+            detected = False
+            previous_biomes = []
+            while not detected:
+                for row in range(self.f_rows):
+                    for col in range(self.f_cols):
+                        if self.full_field[row][col] != self.no_model and not self.full_field[row][col] in previous_biomes:
+                            if len(previous_biomes) < len(biomes) - 1:
+                                current_biome = self.full_field[row][col]
+                                previous_biomes.append(current_biome)
+                                already_spreaded = self.__spread(row, col, current_biome)
+                                if not already_spreaded:
+                                    detected = True
+                            else:
+                                previous_biomes.remove(previous_biomes[0])
+                                current_biome = self.full_field[row][col]
+                                previous_biomes.append(current_biome)
+                                already_spreaded = self.__spread(row, col, current_biome)
+                                if not already_spreaded:
+                                    detected = True
+
+            sub_full = True
+            for row in range(self.f_rows):
+                for col in range(self.f_cols):
+                    if self.full_field[row][col] == self.no_model:
+                        sub_full = False
+                        break
+                if not sub_full:
+                    break
+
+
+        self.show(True)
+        return
+
+    def __spread(self, x, y, symbol):
+        already_spreaded = True
+        if x != 0 and self.full_field[x - 1][y] == self.no_model:
+            self.full_field[x - 1][y] = symbol
+            already_spreaded = False
+        if y != 0 and self.full_field[x][y - 1] == self.no_model:
+            self.full_field[x][y - 1] = symbol
+            already_spreaded = False
+        if x != self.f_rows - 1 and self.full_field[x + 1][y] == self.no_model:
+            self.full_field[x + 1][y] = symbol
+            already_spreaded = False
+        if y != self.f_cols - 1 and self.full_field[x][y + 1] == self.no_model:
+            self.full_field[x][y + 1] = symbol
+            already_spreaded = False
+        return already_spreaded
+
 
     def __biomes_area_count(self):
         biomes_count = random.randint(2, len(self.biomes))
@@ -100,9 +165,9 @@ class field():
 
         sum_biome_area = 0
 
-        for i in range(0, biomes_count):
+        for i in range(biomes_count):
             next_biome = random.choice(self.biomes)
-            while self.symbs_for_biomes in biomes_on_field:
+            while next_biome in biomes_on_field:
                 next_biome = random.choice(self.biomes)
 
             if i != biomes_count - 1:
@@ -117,7 +182,7 @@ class field():
         # print(biomes_count)
         # print(biomes_on_field)
         # print(biomes_area)
-        # print(area)
+        # print(area.keys())
         return area
 
     def __add_character_on_field(self, row, col):
